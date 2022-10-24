9211fc534c88c4444d594a9490b3642cf3c3a36d
ljcana <llanajae.cana@gmail.com>
2022-10-23 19:22:11 -0700

    Setting Parameters
    Setting up parameters of our rocket problem

--- main.py
+++ main.py
@@ -12,3 +12,9 @@ from torch.nn import utils
 import matplotlib.pyplot as plt
 
 logger = logging.getLogger(__name__)
+
+# setting up environment parameters
+
+FRAME_TIME = 0.1    # time interval
+GRAVITY_ACCEL = 0.12    # gravity constant
+BOOST_ACCEL = 0.18  # thrust constant

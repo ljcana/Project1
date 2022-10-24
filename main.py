ed5dfe47f3d74555996dec72f7c502343956e887
ljcana <llanajae.cana@gmail.com>
2022-10-23 20:24:30 -0700

    Set Up Dynamics
    Wrote out the dynamics as shown in the sample code

--- main.py
+++ main.py
@@ -18,3 +18,37 @@ logger = logging.getLogger(__name__)
 FRAME_TIME = 0.1    # time interval
 GRAVITY_ACCEL = 0.12    # gravity constant
 BOOST_ACCEL = 0.18  # thrust constant
+
+
+# Define System Dynamics
+
+class Dynamics(nn.Module):
+
+    def __init__(self):
+        super(Dynamics, self).__init__()
+
+    @staticmethod
+    def forward(state, action):
+
+        """
+        action: thrust or no thrust
+        state[0] = y
+        state[1] = y_dot
+        """
+
+        # Apply gravity
+        delta_state_gravity = t.tensor([0., GRAVITY_ACCEL * FRAME_TIME])
+
+        # Apply Thrust
+        delta_state = BOOST_ACCEL * FRAME_TIME * t.tensor([0., -1.]) * action
+
+        # Update velocity
+        state = state + delta_state + delta_state_gravity
+
+        # Update state
+        step_mat = t.tensor([[1., FRAME_TIME],
+                             [0., 1.]])
+        state = t.matmul(step_mat, state)
+
+        return state
+
